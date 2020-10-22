a31626013561c8e8b06a6b8060b1715749372af6
AlbertSnow <Albert.Snow.Email@gmail.com>
2020-10-22 10:02:27 +0800

    1. finish partial longest palindrome

--- app/src/test/java/com/albert/snow/algorithm/LongestPalindrome.java
+++ app/src/test/java/com/albert/snow/algorithm/LongestPalindrome.java
@@ -7,28 +7,75 @@ public class LongestPalindrome {
 
     @Test
     public void doTest() {
-//        assert 1 == count(1, nums1);
-//        assert 2 == count(2, nums1);
-//        assert 2 == count(3, nums1);
-//        assert 1 == count(4, nums1);
+//        float centerIndex = getLongestIndex("123456899");
+//        System.out.println("MaxIndex: " + centerIndex);
+        String longest = getLongestIndex("12333456899");
+        System.out.println("MaxIndex: " + longest);
+
     }
 
-//    private String count(String str) {
-//        if (str == null || str.length() == 1 ||
-//                (str.length() == 2 && (str.charAt(0) - str.charAt(1)) == 0)) {
-//            return str;
-//        }
-//        int length = str.length();
-//
-//        for (int i = 0; i < length / 2; i ++) {
-//
-//
-//        }
-//    }
-//
-//    private find(int leftIndex,  ) {
-//
-//    }
+    public String getLongestIndex(String array) {
+
+        if (array == null || array.length() <= 0) {
+            return "";
+        }
+
+        int size = array.length();
+        int maxResult = 0;
+        float maxIndex = 0;
+        int maxLeft =0;
+        int maxRight = 0;
+
+        int centerIndex = 0;
+        int iteratorResult = 0;
+
+        if (size == 1) {
+            return array;
+        }
+
+        while (centerIndex >= 0 && centerIndex < size) {
+            int left = centerIndex - 1;
+            int right = centerIndex + 1;
+
+            iteratorResult = 1;
+            while (left > 0 && right < size && (array.charAt(left) == array.charAt(right))) {
+                left--;
+                right++;
+                iteratorResult += 2;
+            }
+
+            if (iteratorResult > maxResult) {
+                maxResult = iteratorResult;
+                maxIndex = centerIndex;
+                maxLeft = ++left;
+                maxRight = ++right;
+            }
+
+            left = centerIndex;
+            right = centerIndex + 1;
+            iteratorResult = 0;
+            while (left > 0 && right < size && (array.charAt(left) == array.charAt(right))) {
+                left--;
+                right++;
+                iteratorResult += 2;
+            }
+
+            if (iteratorResult > maxResult) {
+                maxResult = iteratorResult;
+                maxIndex = centerIndex + 0.5f;
+                maxLeft = ++left;
+                maxRight = ++right;
+            }
+
+
+            centerIndex++;
+        }
+
+
+        String result = array.substring(maxLeft, Math.min(maxRight + 1, size - 1));
+        System.out.println("MaxResult: " + result);
+        return result;
+    }
 
 
 }
